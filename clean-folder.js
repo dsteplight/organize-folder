9e0009dc03a10617fa0e9b9a8e3524668e56075c
dsteplight <dsteplight@gmail.com>
2017-02-06 15:56:41 -0500

    zipping up folders and moving them to zip folder

--- clean-folder.js
+++ clean-folder.js
@@ -4,6 +4,8 @@ const readChunk = require('read-chunk'); // npm install read-chunk
 const fileType = require('file-type');
 const mv = require('mv');
 const path_object = require('path');
+const exec = require('child_process').exec;
+
 var fs = require('fs');
 var Buffer = require('buffer').Buffer;
 
@@ -13,6 +15,7 @@ var config = {};
   config['watched_folder'] = '/Users/dsteplight/Downloads';
   config['jpeg_folder'] = '/Users/dsteplight/Documents/JPEG';
   config['pdf_folder'] = '/Users/dsteplight/Documents/PDF';
+  config['zip_folder'] = '/Users/dsteplight/ZIP';
 
 for (var key in config) 
 {
@@ -61,14 +64,33 @@ watcher
   .on('addDir', function(path) { 
 
    fs.stat(path, function (err, stats){
+
      if (err) {
        //this should always exsits since we are only dealing with directories right now
      }
+
      if (stats.isDirectory()) {
-       // This isn't a directory!
-      const directory_name = path_object.basename(path);
-         console.log("THIS IS A DIRECOTRY THAT WE NEED TO ZIP UP "+ path + " "+file_name);
+      let directory_name = path_object.basename(path);
+      let child = exec("zip -r "+directory_name+".zip "+path,{maxBuffer: 1024 * 500}, function (error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+         else{
+            console.log("THERE SHOULD BE A NEW ZIP FOLDER "+directory_name+".zip");
+            let file_name = directory_name+".zip";
+            let target_folder = config.zip_folder;
+            let targeted_file = target_folder+path_object.sep+file_name;
+
+            //the newly created zip folder will exist in the same folder where this script is running and not in the config.watched_folder path
+            mv(file_name, targeted_file, function(err) {
+                log('This zippped file has been moved to ', targeted_file); 
+            });
+         }
+
+      });
+
      } 
+
    });
 
   })
