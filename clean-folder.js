02b562b8d5b262af0230daf2de385727f9c7f927
dsteplight <dsteplight@gmail.com>
2017-02-07 12:39:21 -0500

    indenting code

--- clean-folder.js
+++ clean-folder.js
@@ -64,43 +64,51 @@ watcher
    })
   .on('addDir', function(path) { 
 
-   fs.stat(path, function (err, stats){
-
-     if (err) {
-       //this should always exsits since we are only dealing with directories right now
-     }
-
-     if (stats.isDirectory()) {
       let directory_name = path_object.basename(path);
-      let child = exec("zip -r "+directory_name+".zip "+path,{maxBuffer: 1024 * 1000}, function (error, stdout, stderr) {
-        if (error !== null) {
-          console.log('exec error: ' + error);
-        }
-         else{
-            console.log("THERE SHOULD BE A NEW ZIP FOLDER "+directory_name+".zip");
-            let file_name = directory_name+".zip";
-            let target_folder = config.zip_folder;
-            let targeted_file = target_folder+path_object.sep+file_name;
+      let file_name = directory_name+".zip";
+      let target_folder = config.zip_folder;
+      let targeted_file = target_folder+path_object.sep+file_name;
 
-            //the newly created zip folder will exist in the same folder where this script is running and not in the config.watched_folder path
-            mv(file_name, targeted_file, function(err) {
-                log('This zippped file has been moved to ', targeted_file); 
-                //now delete the folder from the config.watched_files directory
-                rmdir(path, function (err, dirs, files) 
-                {
-                 //console.log(dirs);
-                 //console.log(files);
-                 console.log('all files are removed from '+ path);
-                });
+         fs.stat(path, function (err, stats)
+         {
 
-            });
-         }
+           if (err) 
+           {
+             //this should always exsits since we are only dealing with directories right now
+           }
 
-      });
+           if (stats.isDirectory()) 
+           {
+            let child = exec("zip -r "+directory_name+".zip "+path,{maxBuffer: 1024 * 1000}, function (error, stdout, stderr) 
+                        {
+                             if (error !== null) 
+                             {
+                               console.log('exec error: ' + error);
+                             }
+                             else
+                             {
+                                 console.log("THERE SHOULD BE A NEW ZIP FOLDER "+directory_name+".zip");
 
-     } 
+                                 //the newly created zip folder will exist in the same folder where this script is running and not in the config.watched_folder path
+                                 mv(file_name, targeted_file, function(err) 
+                                 {
+                                     log('This zippped file has been moved to ', targeted_file); 
+                                     //now delete the folder from the config.watched_files directory
+                                     rmdir(path, function (err, dirs, files) 
+                                     {
+                                      //console.log(dirs);
+                                      //console.log(files);
+                                      console.log('all files are removed from '+ path);
+                                     });
 
-   });
+                                 });
+                              }
+
+                        });
+
+           } 
+
+         });
 
   })
   .on('change', function(path) {
@@ -111,6 +119,7 @@ watcher
   .on('unlinkDir', function(path) { 
   })
   .on('error', function(error) { 
+         console.log('DEBUG: The script caught an error', error);
   })
   .on('ready', function() { log('Initial scan complete. Ready for changes.'); })
 
